66c625e18fb7823f93de2c4dc38490dc16be3a86
Tri-desnz <tribhuwan.dhapola@ics.gov.uk>
2025-09-11 09:40:15 +0100

    SYEIA-41 refactoring for router

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
-import TaskList from './features/TaskList/pages/TaskList';
-import NetworkOperatorDetails from './features/ApplicantInfo/pages/NetworkOperatorDetails';
-import NetworkOperatorContactDetails from './features/ApplicantInfo/pages/NetworkOperatorContactDetails';
-import Workbasket from './features/Workbasket/pages/Workbasket';
-import ApplicationSubmitted from './features/TaskList/pages/ApplicationSubmitted';
+import AppRouter from './routes/AppRouter';
 
 const App = () => (
   <BrowserRouter basename="/syeia/">
     <MainLayout>
-      <Routes>
-        <Route path="/" element={<Navigate to="/workbasket/" replace />} />
-        <Route path="/workbasket/" element={<Workbasket />} />
-        <Route path="/task-list" element={<TaskList />} />
-        <Route path="/network-operator-details" element={<NetworkOperatorDetails />} />
-        <Route path="/network-operator-contact-details" element={<NetworkOperatorContactDetails />} />
-        <Route path="/application-submitted" element={<ApplicationSubmitted />} />
-      </Routes>
+      <AppRouter />
     </MainLayout>
   </BrowserRouter>
 );
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import TaskList from '../features/TaskList/pages/TaskList';
+import NetworkOperatorDetails from '../features/ApplicantInfo/pages/NetworkOperatorDetails';
+import NetworkOperatorContactDetails from '../features/ApplicantInfo/pages/NetworkOperatorContactDetails';
+import Workbasket from '../features/Workbasket/pages/Workbasket';
+import ApplicationSubmitted from '../features/TaskList/pages/ApplicationSubmitted';
 
 const AppRouter: React.FC = () => {
-    return (
-        <div>
-            {/* Empty AppRouter component */}
-        </div>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/workbasket/" replace />} />
+      <Route path="/workbasket/" element={<Workbasket />} />
+      <Route path="/task-list" element={<TaskList />} />
+      <Route path="/network-operator-details" element={<NetworkOperatorDetails />} />
+      <Route path="/network-operator-contact-details" element={<NetworkOperatorContactDetails />} />
+      <Route path="/application-submitted" element={<ApplicationSubmitted />} />
+    </Routes>
+  );
 };
 
 export default AppRouter;
