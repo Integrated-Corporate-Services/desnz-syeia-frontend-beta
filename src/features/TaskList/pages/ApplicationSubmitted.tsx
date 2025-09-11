866fda7750709d7e9afbcd98379645d0757b8ad4
Tri-desnz <tribhuwan.dhapola@ics.gov.uk>
2025-09-11 10:02:36 +0100

    SYEIA-41 refactoring application submission

--- src/features/TaskList/pages/ApplicationSubmitted.tsx
+++ src/features/TaskList/pages/ApplicationSubmitted.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import { APPLICATION_SUBMITTED } from '../../../constants/content';
 
 const ApplicationSubmitted: React.FC = () => (
   <div className="govuk-panel govuk-panel--confirmation">
-    <h1 className="govuk-panel__title">Application submitted</h1>
+    <h1 className="govuk-panel__title">{APPLICATION_SUBMITTED.title}</h1>
     <div className="govuk-panel__body">
-      Your application has been submitted successfully.<br />
-      You will receive a confirmation email shortly.
+      {APPLICATION_SUBMITTED.body}
     </div>
   </div>
 );
