918a793f4971e2e250e37059994193a00655c7c8
Tri-desnz <tribhuwan.dhapola@ics.gov.uk>
2025-09-19 19:02:31 +0100

    SYEIA-215 fix for asset info validation error box width

--- src/features/AssetInfo/pages/AssetInformationForm.tsx
+++ src/features/AssetInfo/pages/AssetInformationForm.tsx
@@ -136,7 +136,7 @@ const AssetInformationForm: React.FC = () => {
       {error && <div className="govuk-error-message">{error}</div>}
       <form className="govuk-!-margin-bottom-6" onSubmit={handleSubmit} noValidate>
         {submitted && Object.keys(errors).length > 0 && (
-          <div className="govuk-error-summary" aria-labelledby="error-summary-title" role="alert" tabIndex={-1} data-module="govuk-error-summary" style={{ marginBottom: '2rem' }}>
+          <div className="govuk-error-summary" aria-labelledby="error-summary-title" role="alert" tabIndex={-1} data-module="govuk-error-summary" style={{ marginBottom: '2rem', maxWidth: 600 }}>
             <h2 className="govuk-error-summary__title" id="error-summary-title">There is a problem</h2>
             <div className="govuk-error-summary__body">
               <ul className="govuk-list govuk-error-summary__list">
