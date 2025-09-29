db05875d30f9786b24e7c328074ce28e8b72dd0c
oluwasorealadelusi-ics <oluwasore.aladelusi@ics.gov.uk>
2025-09-29 15:28:55 +0100

    Model creation for EiaFees

---
+++ src/types/eiaFees.ts
@@ -0,0 +1,11 @@
+export interface EiaFees {
+  eiaId: string;
+  applicationId: string;
+  isEiaDevelopment: boolean;
+  requiresFullEia: boolean;
+  screeningOnly: boolean;
+  createdAt: string;
+  updatedAt: string;
+  createdBy: string;
+  updatedBy: string;
+}
