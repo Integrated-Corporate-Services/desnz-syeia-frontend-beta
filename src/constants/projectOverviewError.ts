062602f603159357360ca67a24124501f1306109
saikeerthi-ics <saikeerthi.maddineni@ics.gov.uk>
2025-10-27 15:52:24 +0000

    Bug/syeia 429 project overview UI changes (#23)

---
+++ src/constants/projectOverviewError.ts
@@ -0,0 +1,14 @@
+// Error messages for ProjectOverview feature
+export const PROJECT_OVERVIEW_ERRORS = {
+  PROJECT_NAME_REQUIRED: "Enter the project name",
+  PROJECT_DESCRIPTION_REQUIRED: "Enter the project description",
+  TALLEST_POLE_HEIGHT_REQUIRED: "Enter the tallest pole height",
+  PLAN_REFERENCE_REQUIRED: "Enter the plan reference",
+  WORK_START_DATES_REQUIRED: "Select whether work start dates are known",
+  EARLIEST_DATE_INVALID: "Earliest expected start date must be a real year",
+  LATEST_DATE_INVALID: "Latest expected start date must be a real year",
+  RELATED_APPLICATIONS_REQUIRED: "Select yes if there are related applications",
+  RELATED_APPLICATIONS_ADD: "Add one or more related applications",
+  RELATED_CPO_REQUIRED: "Select yes if there is a related CPO",
+  RELATED_CPO_DETAILS_REQUIRED: "Enter the details of the related CPO",
+};
