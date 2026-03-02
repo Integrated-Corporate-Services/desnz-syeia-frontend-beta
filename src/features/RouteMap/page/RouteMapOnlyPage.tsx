066da341c2093622b3cb77a2a22caeb9d120dc83
SauravkIcs2582 <Saurav.Kumar@ics.gov.uk>
2026-03-02 21:19:26 +0000

    SYEIA-238-Fix-DocumentDownload-OpenMapInNewTab

--- src/features/RouteMap/page/RouteMapOnlyPage.tsx
+++ src/features/RouteMap/page/RouteMapOnlyPage.tsx
@@ -26,7 +26,7 @@ const RouteMapOnlyPage: React.FC = () => {
     }));
 
   return (
-    <div style={{ width: "38vw", height: "60vh", background: "#fff" }}>
+    <div style={{ width: 960, height: 500, background: "#fff" }}>
       <SensitiveAreaCheckMap
         routes={transformedRoutes}
         mode="overview"
