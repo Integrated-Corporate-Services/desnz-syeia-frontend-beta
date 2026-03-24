62b3eab83b04e8ef0ebe0d33cd92055280639a57
SauravkIcs2582 <Saurav.Kumar@ics.gov.uk>
2026-03-24 13:07:55 +0000

    SYEIA-1538: Fix the bugs

--- src/features/PostConsultation/pages/PostConsultationLpaAgreement.tsx
+++ src/features/PostConsultation/pages/PostConsultationLpaAgreement.tsx
@@ -13,7 +13,7 @@ import { SaveType } from "../types";
 import { useNavigate } from "react-router-dom";
 
 const PostConsultationLpaAgreement: React.FC = () => {
-  const { applicationId, getCheckYourAnswersUrl, getTaskListUrl } = usePostConsultationNavigation();
+  const { applicationId, getCheckYourAnswersUrl, getTaskListUrl, navigateAfterCompletion } = usePostConsultationNavigation();
   const navigate = useNavigate();
   const {
     lpaModifications,
@@ -35,7 +35,7 @@ const PostConsultationLpaAgreement: React.FC = () => {
       return;
     }
     if (success && lpaModifications === "no") {
-      navigate(getCheckYourAnswersUrl());
+      navigateAfterCompletion();
       return;
     }
   };
