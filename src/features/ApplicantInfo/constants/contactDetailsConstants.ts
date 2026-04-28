7f9e35cae6cb2e10e9d14ded8db9d0e9a90a6bba
skuses <Sagar.Kumar@ics.gov.uk>
2026-04-28 09:02:01 +0100

    SYEIA-1573 INCORRECT DETAILS content update

--- src/features/ApplicantInfo/constants/contactDetailsConstants.ts
+++ src/features/ApplicantInfo/constants/contactDetailsConstants.ts
@@ -27,5 +27,5 @@ export const LABELS = {
 
 export const CONDITIONAL_TEXT = {
   INCORRECT_DETAILS:
-    "If any details are missing or incorrect then the named contact person must update their account details on the Energy Information Portal (EIP). You cannot submit this application if any details are missing.",
+    "If anything is missing or incorrect then the named contact person or a supervisor must update these contact details. You cannot submit this application if any details are missing.",
 };
