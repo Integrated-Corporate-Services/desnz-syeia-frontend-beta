73cfc9e312efda9f5284295abc559bf8326d5fb2
sushantchoudhari3 <sushant.choudhari@ics.gov.uk>
2026-05-09 21:18:59 +0100

    SYEIA-1472- related application in check your answer and summary page
     related application in check your answer and summary page

--- src/features/CheckYourAnswers/pages/CheckYourAnswers.tsx
+++ src/features/CheckYourAnswers/pages/CheckYourAnswers.tsx
@@ -775,9 +775,28 @@ const CheckYourAnswers: React.FC = () => {
                       </ul>
                     </dd>
                   </div>
+                  <div className="govuk-summary-list__row">
+                    <dt className="govuk-summary-list__key">
+                      Related applications
+                    </dt>
+                    <dd className="govuk-summary-list__value">
+                      {projectDetails?.has_related_applications === "true" ? "Yes" : projectDetails?.has_related_applications === "false" ? "No" : "-"}
+                    </dd>
+                  </div>
+                  {projectDetails?.has_related_applications === "true" && projectDetails?.related_applications_details && (
+                    <div className="govuk-summary-list__row">
+                      <dt className="govuk-summary-list__key">
+                        Related application details
+                      </dt>
+                      <dd className="govuk-summary-list__value" style={{ whiteSpace: 'pre-wrap' }}>
+                        {projectDetails.related_applications_details}
+                      </dd>
+                    </div>
+                  )}
                 </dl>
               </div>
             </div>
+
             {/* Assets summary card */}
             <div className="govuk-summary-card">
               <div className="govuk-summary-card__title-wrapper">
