e19b435f0848b707e9987ec1409400133c687a10
sushantchoudhari3 <sushant.choudhari@ics.gov.uk>
2026-05-12 10:16:07 +0100

    SYEIA-1833-bacs payment removed unwanted link
    bacs payment removed unwanted link

--- src/features/Payments/pages/BankTransferSuccessPage.tsx
+++ src/features/Payments/pages/BankTransferSuccessPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate, useLocation, Link } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { S37_BASE_URL } from '../../../constants/s37';
 import { useGetApplicationId } from '../../../hooks/useGetApplicationId';
 
