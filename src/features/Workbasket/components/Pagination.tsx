9a02134269cdb37a170f162f20c708f7c5943d54
harsh29-ics <Harsh.Vasudev@ics.gov.uk>
2026-05-29 09:43:17 +0100

    Syeia 1886 pagination alignment (#266)

--- src/features/Workbasket/components/Pagination.tsx
+++ src/features/Workbasket/components/Pagination.tsx
@@ -79,6 +79,7 @@ export const Pagination: React.FC<PaginationProps> = ({
             >
               <path d="m6.5938-0.0078125-6.7266 6.7266 6.7441 6.4062 1.377-1.449-4.1856-3.9768h12.896v-2h-12.984l4.2931-4.293-1.414-1.414z"></path>
             </svg>
+            <span className="app-pagination-short-label" aria-hidden="true">Prev</span>
             <span className="govuk-pagination__link-title">
               Previous<span className="govuk-visually-hidden"> page</span>
             </span>
@@ -148,6 +149,7 @@ export const Pagination: React.FC<PaginationProps> = ({
             <span className="govuk-pagination__link-title">
               Next<span className="govuk-visually-hidden"> page</span>
             </span>
+            <span className="app-pagination-short-label" aria-hidden="true">Next</span>
             <svg
               className="govuk-pagination__icon govuk-pagination__icon--next"
               xmlns="http://www.w3.org/2000/svg"
