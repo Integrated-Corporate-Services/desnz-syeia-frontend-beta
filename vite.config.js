457c55d9430d336a07e61a381540423d5f6f4760
Tri-desnz <tribhuwan.dhapola@ics.gov.uk>
2026-06-06 14:45:16 +0100

    SYEIA-1900 changes for favicon and log

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,14 @@ export default defineConfig(({ mode }) => {
     return {
         base: '/frontend',
         plugins: [react()],
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    quietDeps: true, // Suppress deprecation warnings from dependencies
+                    silenceDeprecations: ['import', 'global-builtin', 'color-functions', 'slash-div', 'mixed-decls'],
+                }
+            }
+        },
         test: {
             environment: 'jsdom',
             globals: true,
