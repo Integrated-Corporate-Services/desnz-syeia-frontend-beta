96572d64ccec47064eb2fdc0538af3197e739885
Kumar <sagar.kumar@ics.gov.uk>
2026-06-16 09:24:41 +0100

    SYEIA-1658 code ql build error fixes

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,11 @@ export default defineConfig(({ mode }) => {
     return {
         base: '/frontend',
         plugins: [react()],
+        build: {
+            target: 'esnext',
+            minify: 'esbuild',
+            cssMinify: false,
+        },
         css: {
             preprocessorOptions: {
                 scss: {
