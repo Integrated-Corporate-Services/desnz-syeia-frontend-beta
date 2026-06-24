938d8412e4da67a394410a890cf64a615417f7fe
Kumar <sagar.kumar@ics.gov.uk>
2026-06-24 17:15:54 +0100

    SYEIA-2100 application summary ui enhancements

--- src/features/ApplicationSummary/pages/ApplicationSummaryPage.tsx
+++ src/features/ApplicationSummary/pages/ApplicationSummaryPage.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import { NWL_BASE_URL } from '../../../constants/nwl';
+import { useParams } from 'react-router-dom';
 import { APPLICATION_SUMMARY_CONSTANTS as CONSTANTS } from '../constants';
 import { fetchApplicationReviewSummary } from '../services';
 import { useWithdrawalRequest } from '../hooks';
@@ -8,31 +7,10 @@ import { useDocumentDownload } from '../../NWL/CheckYourAnswers/hooks';
 import { ApplicationReviewSummaryData } from '../types/reviewSummary';
 import {
     TaskListSummaryBreadcrumbs,
-    ReviewApplicationInfoCard,
-    ReviewPaymentDetailsCard,
-    WithdrawalNotificationBanner,
-    SummaryWithdrawButton,
+    ApplicationSummaryContent,
 } from '../components';
 
-import {
-    ApplicantDetailsSummaryCard,
-    NWLApplicationDetailsSummaryCard,
-    OccupierDetailsSummaryCard,
-    LandownerDetailsSummaryCard,
-    RepresentativeDetailsSummaryCard,
-    SiteAddressSummaryCard,
-    LandRegistrySummaryCard,
-    OSGridReferenceSummaryCard,
-    IdentifyingInformationSummaryCard,
-    AssetSummaryCard,
-    AssetsPlanSummaryCard,
-    NegotiationsSummaryCard,
-    NWLAdditionalInformationSummaryCard,
-} from '../../NWL/CheckYourAnswers/components';
-import { CHECK_YOUR_ANSWERS_CONSTANTS as CYA_CONSTANTS } from '../../NWL/CheckYourAnswers/constants';
-
 export const ApplicationSummaryPage: React.FC = () => {
-    const navigate = useNavigate();
     const { applicationId } = useParams<{ applicationId: string }>();
     const { withdrawalRequest } = useWithdrawalRequest(applicationId);
     
@@ -55,7 +33,7 @@ export const ApplicationSummaryPage: React.FC = () => {
             try {
                 setLoading(true);
                 setError(null);
-                const result = await fetchApplicationReviewSummary(applicationId, 'NWL');
+                const result = await fetchApplicationReviewSummary(applicationId);
                 if (isMounted) {
                     setData(result);
                 }
@@ -108,135 +86,17 @@ export const ApplicationSummaryPage: React.FC = () => {
         );
     }
 
-    const showWithdraw =
-        data.permissions?.canWithdraw && !data.permissions?.canEdit && !withdrawalRequest;
-
     return (
         <div className="govuk-width-container">
             <TaskListSummaryBreadcrumbs applicationId={applicationId!} />
 
-            <main className="govuk-main-wrapper" id="main-content" role="main">
+            <main className="govuk-main-wrapper govuk-!-padding-top-2" id="main-content" role="main">
                 <div className="govuk-grid-row">
                     <div className="govuk-grid-column-two-thirds">
-                        {withdrawalRequest?.request_status === 'Requested' && (
-                            <WithdrawalNotificationBanner />
-                        )}
-
-                        <h1 className="govuk-heading-xl">{CONSTANTS.REVIEW_LAYOUT.HEADING}</h1>
-
-                        <ReviewApplicationInfoCard
-                            desnzRef={data.desnzRef}
-                            status={data.status}
-                            withdrawalRequest={withdrawalRequest}
-                        />
-
-                        <ReviewPaymentDetailsCard payment={data.payment} />
-
-                        {showWithdraw && (
-                            <SummaryWithdrawButton
-                                onClick={() =>
-                                    navigate(`${NWL_BASE_URL}/${applicationId}/withdraw`, {
-                                        state: {
-                                            desnzRef: data.desnzRef,
-                                            formType: 'NWL',
-                                        },
-                                    })
-                                }
-                            />
-                        )}
-
-                        <h2 className="govuk-heading-l">{CYA_CONSTANTS.SECTION_HEADINGS.APPLICANT_DETAILS}</h2>
-                        <ApplicantDetailsSummaryCard 
-                            data={data.applicantDetails} 
-                            applicationId={applicationId!} 
-                            canEdit={false} 
-                        />
-
-                        <h2 className="govuk-heading-l">{CYA_CONSTANTS.SECTION_HEADINGS.APPLICATION_DETAILS}</h2>
-                        <NWLApplicationDetailsSummaryCard 
-                            data={data.applicationDetails} 
-                            noticeComplianceData={data.noticeCompliance}
-                            applicationId={applicationId!} 
-                            canEdit={false} 
-                        />
-
-                        <h2 className="govuk-heading-l">{CYA_CONSTANTS.SECTION_HEADINGS.OWNER_OCCUPIER_DETAILS}</h2>
-                        <OccupierDetailsSummaryCard 
-                            data={data.objectorDetails} 
-                            applicationId={applicationId!} 
-                            canEdit={false} 
-                        />
-
-                        <LandownerDetailsSummaryCard 
-                            data={data.landownerDetails} 
-                            applicationId={applicationId!} 
-                            canEdit={false} 
-                        />
-
-                        <RepresentativeDetailsSummaryCard 
-                            data={data.representativeDetails} 
-                            applicationId={applicationId!} 
-                            canEdit={false} 
-                        />
-
-                        <h2 className="govuk-heading-l">{CYA_CONSTANTS.SECTION_HEADINGS.LAND_DETAILS}</h2>
-                        <SiteAddressSummaryCard 
-                            data={data.landDetails} 
-                            applicationId={applicationId!} 
-                            canEdit={false} 
-                        />
-
-                        <LandRegistrySummaryCard 
-                            data={data.landDetails} 
-                            applicationId={applicationId!} 
-                            canEdit={false} 
-                        />
-
-                        <OSGridReferenceSummaryCard 
-                            data={data.landDetails} 
-                            applicationId={applicationId!} 
-                            canEdit={false} 
-                        />
-
-                        <IdentifyingInformationSummaryCard 
-                            data={data.landDetails} 
-                            applicationId={applicationId!} 
-                            canEdit={false} 
-                        />
-
-                        <h2 className="govuk-heading-l">{CYA_CONSTANTS.SECTION_HEADINGS.ASSETS}</h2>
-                        {data.assets && data.assets.length > 0 && data.assets.map((asset, index) => (
-                            <AssetSummaryCard
-                                key={asset.asset_id || index}
-                                asset={asset}
-                                assetNumber={index + 1}
-                                applicationId={applicationId!}
-                                canEdit={false}
-                            />
-                        ))}
-
-                        <AssetsPlanSummaryCard
-                            data={data.assetsMetadata}
+                        <ApplicationSummaryContent
+                            data={data}
                             applicationId={applicationId!}
-                            canEdit={false}
-                        />
-
-                        {data.negotiations && (
-                            <>
-                                <h2 className="govuk-heading-l">{CYA_CONSTANTS.SECTION_HEADINGS.NEGOTIATIONS}</h2>
-                                <NegotiationsSummaryCard 
-                                    data={data.negotiations} 
-                                    applicationId={applicationId!} 
-                                    canEdit={false} 
-                                />
-                            </>
-                        )}
-
-                        <h2 className="govuk-heading-l">{CYA_CONSTANTS.SECTION_HEADINGS.ADDITIONAL_INFORMATION}</h2>
-                        <NWLAdditionalInformationSummaryCard 
-                            data={data.additionalInformation} 
-                            applicationId={applicationId!} 
-                            canEdit={false} 
+                            withdrawalRequest={withdrawalRequest}
                         />
                     </div>
                 </div>
---
+++ src/features/NWL/ApplicationSummary/components/index.ts
@@ -0,0 +1,1 @@
+export { NWLApplicationSummaryContent } from './NWLApplicationSummaryContent';
