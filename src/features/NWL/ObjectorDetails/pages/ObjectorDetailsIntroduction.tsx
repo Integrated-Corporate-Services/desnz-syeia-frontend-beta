c1575e32bf11a573fb665f096afc60f6282b8682
saikeerthi-ics <saikeerthi.maddineni@ics.gov.uk>
2026-06-25 13:26:07 +0100

    Feature/dev observations for nwl (#347)

--- src/features/NWL/ObjectorDetails/pages/ObjectorDetailsIntroduction.tsx
+++ src/features/NWL/ObjectorDetails/pages/ObjectorDetailsIntroduction.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useGetApplicationId } from "../../../../hooks/useGetApplicationId";
-import { useNWLProgress } from '../../hooks/useNWLProgress';
 import { NWL_BASE_URL } from "../../../../constants/nwl";
 import {
   BREADCRUMBS,
@@ -16,15 +15,10 @@ import {
 const ObjectorDetailsIntroduction: React.FC = () => {
   const navigate = useNavigate();
   const appId = useGetApplicationId();
-  const { updateProgress } = useNWLProgress(appId);
 
   const handleContinue = () => {
     (async () => {
-      try {
-        await updateProgress('Objector details', 'Not completed');
-      } catch (e) {
-        // ignore progress errors
-      }
+      
       navigate(`${NWL_BASE_URL}/${appId}/objector-details`);
     })();
   };
