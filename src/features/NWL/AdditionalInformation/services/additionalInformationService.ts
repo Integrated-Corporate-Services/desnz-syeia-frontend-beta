cf6f0da30c646b88019be0e2a898437fdb98bedd
SauravkIcs2582 <Saurav.Kumar@ics.gov.uk>
2026-08-03 21:11:57 +0100

    SYEIA-2309-fix-the-unused-variables-imports (#441)

--- src/features/NWL/AdditionalInformation/services/additionalInformationService.ts
+++ src/features/NWL/AdditionalInformation/services/additionalInformationService.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from 'axios';
+import axios from 'axios';
 import { AdditionalInformationData } from '../types';
 import { UploadedFile, ApplicationDocument } from '../../../../types/fileUpload';
 import { parseBackendValidationErrors } from '../../../../utils/apiErrorHandler';
