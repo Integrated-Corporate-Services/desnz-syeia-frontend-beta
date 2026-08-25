3a3f51bd4f6438a76fa02bfc66d78d83548381d2
SauravkIcs2582 <Saurav.Kumar@ics.gov.uk>
2026-08-25 12:40:41 +0100

    SYEIA-2484-Total-Amount-Structure-frontend (#472)

--- src/features/Payments/pages/PaymentMethodPage.tsx
+++ src/features/Payments/pages/PaymentMethodPage.tsx
@@ -202,7 +202,7 @@ const handlePayByCard = async () => {
             <h1 className="govuk-heading-xl">{PAYMENT_PAGE_TEXT.pageTitle}</h1>
 
             <p className="govuk-body">
-              You must pay <strong>Â£{effectiveTotalAmount?.toFixed(2) ?? '0.00'}</strong> to submit this application.
+              You must pay <strong>£{effectiveTotalAmount?.toFixed(2) ?? '0.00'}</strong> to submit this application.
             </p>
 
             <p className="govuk-body">{PAYMENT_PAGE_TEXT.cardRedirect}</p>
