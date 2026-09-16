f6e6f342c21ef98fb121c4676caf76230a090894
SauravkIcs2582 <Saurav.Kumar@ics.gov.uk>
2026-09-16 10:28:25 +0100

    SYEIA-1490-fix-the-feature-flag

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -95,7 +95,7 @@ class ConfigService {
           },
         },
         dnoTeamCoordinatorsOrganisationsEnabled: parseEnvBoolean(
-          getRuntimeEnv('DNO_TEAM_COORDINATORS_ORGANISATIONS_ENABLED')
+          getRuntimeEnv('VITE_DNO_TEAM_COORDINATORS_ORGANISATIONS_ENABLED', 'false')
         ),
         manageUserRoleChange: {
           enabled: parseEnvBoolean(getRuntimeEnv('VITE_ENABLE_MANAGE_USER_ROLE_CHANGE', 'false')),
